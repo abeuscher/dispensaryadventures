29458b5a72da402b70d9a83bda15c8224770ef99
Alex Beuscher <alexbeuscher@gmail.com>
2020-01-31 01:56:56 -0800

    Continuig improvments. This is a bad commit history so far

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ var siteSettings = {
 var parseHTML = require("./utils/parse-html.js");
 
 var widgetTemplates = {
-
+  "sortTable": require("./sort-table/table.pug")
 };
 var ReviewCarousel = require("./page-elements/review-carousel");
 var Magnifier = require("./magnifier/");
@@ -30,30 +30,42 @@ window.addEventListener("load", function () {
 });
 
 var siteActions = [{
-  "element":".sortable",
-  "action":function(els) {
+  "element": "#product-table-sortable",
+  "action": function (els) {
+    console.log(productData);
+    if (productData) {
+      els[0].appendChild(parseHTML(widgetTemplates.sortTable(productData)));
+    }
     SortTable(els);
   }
-},{
-  "element":".zoom-icon",
-  "action":function(els) {
+}, {
+  "element": ".zoom-icon",
+  "action": function (els) {
     for (i = 0; i < els.length; i++) {
-      els[i].addEventListener("click", function(e) {
+      els[i].addEventListener("click", function (e) {
         e.preventDefault();
+        console.log(e.target.parentNode);
         var lens = e.target.parentNode.querySelectorAll(".lens")[0];
+        var slide = e.target.parentNode.querySelectorAll(".slide-image")[0];
+
         if (lens) {
+          dropLens();
+        }
+        function dropLens(e) {
           if (lens.classList.contains("active")) {
             lens.classList.remove("active");
+            slide.removeEventListener("mouseleave", dropLens);
           }
           else {
             lens.classList.add("active");
+
+            slide.addEventListener("mouseleave", dropLens);
           }
-          
         }
-      })
+      });
     }
   }
-},{
+}, {
   "element": ".magnify",
   "action": function (els) {
     for (i = 0; i < els.length; i++) {
@@ -69,4 +81,7 @@ var siteActions = [{
 }, {
   "element": "[data-bg-array]",
   "action": require("./utils/data-bg-array")
+}, {
+  "element": "#more-posts",
+  "action": require("./get-posts/")
 }];
--- src/js/sort-table/index.js
+++ src/js/sort-table/index.js
@@ -4,24 +4,32 @@ function SortTable(tables) {
         for (b = 0; b < buttons.length; b++) {
             buttons[b].addEventListener("click", function (e) {
                 e.preventDefault();
-                var s = this.getAttribute("data-sort").split("|");
+                var sortField = this.getAttribute("data-sort");
+                var sortDirection = this.getAttribute("data-direction") == 'true';
+                var sortType = this.getAttribute("data-type");
                 var headers = this.parentNode.parentNode.parentNode.children;
-                for (z=0;z<headers.length;z++) {
+                for (z = 0; z < headers.length; z++) {
                     var h = headers[z];
-                    if (h!=this.parentNode.parentNode) {
+                    if (h != this.parentNode.parentNode) {
                         h.classList.remove("active");
                     }
                     else {
-                        h.classList.add("active"); 
+                        h.classList.add("active");
                     }
-                    
                 }
-                doSort((s[1]=='true'),s[0],this.parentNode.parentNode.parentNode.parentNode.parentNode);
+                doSort(sortDirection, sortField, this.parentNode.parentNode.parentNode.parentNode.parentNode, sortType);
+                clearActive(buttons);
+                this.classList.add("active");
             });
+        }
+        function clearActive(els) {
+            for (b = 0; b < els.length; b++) {
+                els[b].classList.remove("active");
+            }
         }
     }
 }
-function doSort(ascending, columnClassName, el) {
+function doSort(ascending, columnClassName, el, dataType) {
     var tbody = el.getElementsByTagName("tbody")[0];
     var rows = tbody.getElementsByTagName("tr");
     var unsorted = true;
@@ -32,20 +40,38 @@ function doSort(ascending, columnClassName, el) {
             var nextRow = rows[r + 1];
             var value = row.getElementsByClassName(columnClassName)[0].innerHTML;
             var nextValue = nextRow.getElementsByClassName(columnClassName)[0].innerHTML;
-            value = value.replace(',', '');
-            value = value.replace('$','');
-            nextValue = nextValue.replace(',', '');
-            nextValue = nextValue.replace('$', '');
-            if (!isNaN(value)) {
-                value = parseFloat(value);
-                nextValue = parseFloat(nextValue);
+            switch (dataType) {
+                case "number":
+                    value = cleanNumber(value);
+                    nextValue = cleanNumber(nextValue);
+                    if (!ascending ? value > nextValue : value < nextValue) {
+                        tbody.insertBefore(nextRow, row);
+                        unsorted = true;
+                    }
+                    break;
+                case "date":
+                    value = new Date(value);
+                    nextValue = new Date(nextValue);  
+                    if (!ascending ? value > nextValue : value < nextValue) {
+                        tbody.insertBefore(nextRow, row);
+                        unsorted = true;
+                    }
+                    break;
+                default:
+                    if (ascending ? value > nextValue : value < nextValue) {
+                        tbody.insertBefore(nextRow, row);
+                        unsorted = true;
+                    }
+                    break;
             }
-            if (ascending ? value > nextValue : value < nextValue) {
-                tbody.insertBefore(nextRow, row);
-                unsorted = true;
-            }
+
         }
     }
+}
+function cleanNumber(n) {
+    n = n.replace("$", "");
+    n = n.replace("%", "");
+    return parseFloat(n);
 }
 
 module.exports = SortTable;
