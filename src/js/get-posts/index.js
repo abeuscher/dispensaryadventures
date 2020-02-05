58867c7a5ec00f693e891371e202035c03a02a16
Alex Beuscher <alexbeuscher@gmail.com>
2020-02-05 15:38:53 -0800

    I mean I made some changes to the files, mmmkay?

--- src/js/get-posts/index.js
+++ src/js/get-posts/index.js
@@ -14,7 +14,7 @@ function GetPosts() {
             var bucket = document.getElementById("post-bucket");
             var nextPage = parseInt(window.location.hash.split("-")[1]);
             if (nextPage<=maxPages) {
-                var fetchUrl = "/wp-json/wp/v2/posts?per_page=9&page="+nextPage;
+                var fetchUrl = (window.location.hostname.indexOf(".local") > -1 ? "http://" : "https://") + window.location.hostname + "/wp-json/wp/v2/posts?per_page=9&page="+nextPage;
                 fetch(fetchUrl)
                 .then(data => {
                     return data.json();
